3f27e0d1d9926b26931179afbb69e6704e8dd82c
aakankshadb <132448359+aakankshadb@users.noreply.github.com>
2023-05-12 17:33:07 +0530

    Update git_assignment.py
    added code to pull from origin

--- git_assignments/git_assignment.py
+++ git_assignments/git_assignment.py
@@ -1,2 +1,4 @@
 for i in the range(1,5):
-    print(i)
+    print(i)//adding the code in local and push to origin
+for i in the range(1,5):
+    print(i*i)//adding the code in origin and pulling it from local
