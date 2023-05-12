556b11feef4abd30924d524cb48946d2006f35e9
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-12 19:28:22 +0530

    Modified the file

--- git_assignments/git_assignment.py
+++ git_assignments/git_assignment.py
@@ -6,4 +6,6 @@ for i in range(1,5):
     print(i*2) #adding the code in feature branch then will merge it
 for i in range(1,5):
     print("Hello")
+for i in range(0,6):
+    print("Good evening!")
 
