a79739e7c70f96329bcf7298e9ea78729a08b125
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-12 20:48:39 +0530

    Added to print one

--- git_assignments/git_assignment.py
+++ git_assignments/git_assignment.py
@@ -4,6 +4,7 @@ for i in range(1,5):
     print(i*i) #adding the code in origin and pulling it from local
 for i in range(1,5):
     print(i*2) #adding the code in feature branch then will merge it
+print("one")
 
 
 
