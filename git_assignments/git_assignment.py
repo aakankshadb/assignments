81729646d8d07a80bca5a9ebd7b9c2df07eb91e0
aakankshadb <132448359+aakankshadb@users.noreply.github.com>
2023-05-12 18:02:30 +0530

    Update git_assignment.py

--- git_assignments/git_assignment.py
+++ git_assignments/git_assignment.py
@@ -1,4 +1,4 @@
-for i in the range(1,5):
-    print(i)#adding the code in local and push to origin
-for i in the range(1,5):
-    print(i*i)#adding the code in origin and pulling it from local
+for i in range(1,5):
+    print(i) #adding the code in local and push to origin
+for i in range(1,5):
+    print(i*i) #adding the code in origin and pulling it from local
