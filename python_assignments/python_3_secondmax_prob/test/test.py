6945b17f42fad9db74bfdd8f120a97528aa7d8ea
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-19 12:56:16 +0530

    This is utils,driver and test file of 3rd problem

--- python_assignments/python_3_secondmax_prob/test/test.py
+++ python_assignments/python_3_secondmax_prob/test/test.py
@@ -1,10 +1,12 @@
 import unittest
-
+from python_assignments.python_3_secondmax_prob.core.utils import *
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        actualoutput=second_max(5,[2,3,6,5,8])
+        expectedoutput=6
+        self.assertEqual(actualoutput, expectedoutput)  # add assertion here
 
 
 if __name__ == '__main__':
