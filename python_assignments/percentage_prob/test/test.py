6409e1a030c9d9c4fb9717871dffb990ac6a97c6
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-19 15:23:32 +0530

    Renaming the file

--- python_assignments/percentage_prob/test/test.py
+++ python_assignments/percentage_prob/test/test.py
@@ -1,5 +1,5 @@
 import unittest
-from python_assignments.python_2_percentage_prob.core.utils import *
+from python_assignments.percentage_prob.core.utils import *
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         actualinput = percent_calc({'s1': [90, 90, 90], 's2': [10, 60, 250]}, 's1')
