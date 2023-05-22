6e2866a02eae786ea68bc181ae3fb169c941a0e3
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-23 00:47:02 +0530

    Adding driver,utils,test file

---
+++ python_assignments/lists/core/driver.py
@@ -0,0 +1,6 @@
+from python_assignments.lists.core.utils import *
+n=12
+command=["insert 0 5","insert 1 10","insert 0 6","print","remove 6","append 9","append 1","sort","print","pop"
+    ,"reverse","print"]
+lis=[]
+list_func(n,command,lis)
