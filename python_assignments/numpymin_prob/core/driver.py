45ac0cf1a97f8b81a56f6ec5664cd5832851c6b5
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-22 22:36:39 +0530

    Adding driver,utils,test file

---
+++ python_assignments/numpymin_prob/core/driver.py
@@ -0,0 +1,5 @@
+from python_assignments.numpymin_prob.core.utils import *
+N=4 #No of rows in the array
+M=2 #No of columns in array
+my_array=[[2,5],[3,7],[1,3],[4,0]]
+minimum(my_array)
---
+++ python_assignments/numpymin_prob/core/utils.py
@@ -0,0 +1,3 @@
+import numpy
+def minimum(my_array):
+    print(numpy.max(numpy.min(my_array, axis=1)))
