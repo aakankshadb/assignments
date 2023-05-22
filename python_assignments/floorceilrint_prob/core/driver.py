696f16ec5cb4a1c1bff3c5c82f65593baf9ecee2
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-22 21:54:50 +0530

    Adding driver,utils,test file

---
+++ python_assignments/floorceilrint_prob/core/driver.py
@@ -0,0 +1,5 @@
+from python_assignments.floorceilrint_prob.core.utils import *
+my_array=np.array([1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9])
+floors(my_array)
+ceils(my_array)
+rints(my_array)
---
+++ python_assignments/floorceilrint_prob/core/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def floors(array):
+	np.set_printoptions(legacy='1.13')
+	print(np.floor(array))
+def ceils(array):
+	np.set_printoptions(legacy='1.13')
+	print(np.ceil(array))
+def rints(array):
+	np.set_printoptions(legacy='1.13')
+	print(np.rint(array))
