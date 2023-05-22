698db1a6da53e91881b034bacfbab09cfa5a257b
aakanshdb <aakanksha.shrivas@diggibyte.com>
2023-05-23 00:04:04 +0530

    Adding driver,utils,test file

---
+++ python_assignments/wordorder_prob/core/utils.py
@@ -0,0 +1,12 @@
+def count_words(n,words):
+     d = {}
+     for word in words:
+          key = word
+          if key not in d:
+               d[key] = 1
+          else:
+               d[key] += 1
+
+     print(len(d))
+     for key, val in d.items():
+          print(val, end=' ')
